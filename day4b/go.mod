5ff60b595bd3417f99796ae0f7240a00613554f0
Anton Egorov <aegorov@aligntech.com>
2025-12-04 11:08:58 +0300

    (+) day4b

---
+++ day4b/go.mod
@@ -0,0 +1,3 @@
+module day4a
+
+go 1.25.4
---
+++ day4b/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func checkGridConsistent(grid []string) bool {
+	if len(grid) == 0 {
+		return true
+	}
+	width := len(grid[0])
+	for _, row := range grid {
+		if len(row) != width {
+			return false
+		}
+	}
+	return true
+}
+
+func hasRoll(grid []string, x, y, width, height int) bool {
+	if x < 0 || x >= width || y < 0 || y >= height {
+		return false
+	}
+	return grid[y][x] == '@'
+}
+
+func getNeighbors(grid []string, x, y, width, height int) int {
+	directions := [][2]int{
+		{-1, -1}, {0, -1}, {1, -1},
+		{-1, 0}, {1, 0},
+		{-1, 1}, {0, 1}, {1, 1}}
+
+	neighbors := 0
+	for dir := range directions {
+		dx := directions[dir][0]
+		dy := directions[dir][1]
+		nx := x + dx
+		ny := y + dy
+		if hasRoll(grid, nx, ny, width, height) {
+			neighbors++
+		}
+	}
+
+	return neighbors
+}
+
+func removeRoll(grid []string, x, y int) {
+	row := []rune(grid[y])
+	row[x] = '.'
+	grid[y] = string(row)
+}
+
+func run(grid []string) int {
+	if !checkGridConsistent(grid) {
+		panic("Inconsistent grid")
+	}
+
+	width := len(grid[0])
+	height := len(grid)
+	if width == 0 || height == 0 {
+		return 0
+	}
+
+	rolls := 0
+	removed := true
+	grid_copy := make([]string, height)
+	copy(grid_copy, grid)
+
+	for removed {
+		removed = false
+		copy(grid, grid_copy)
+
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				if !hasRoll(grid, x, y, width, height) {
+					continue
+				}
+
+				neighbors := getNeighbors(grid, x, y, width, height)
+				if neighbors < 4 {
+					removed = true
+					rolls++
+					removeRoll(grid_copy, x, y)
+				}
+			}
+		}
+	}
+
+	return rolls
+}
+
+func test(grid []string, exp_rolls int) bool {
+
+	rolls := run(grid)
+
+	if rolls == exp_rolls {
+		fmt.Printf("✅Test passed: %v\n", grid)
+		return true
+	} else {
+		fmt.Printf("❌Test failed: %v\n", grid)
+		fmt.Printf("Actual total: %d\n", rolls)
+		fmt.Printf("Expected total: %d\n", exp_rolls)
+		return false
+	}
+}
+
+func readInput(filename string) []string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	data = bytes.ReplaceAll(data, []byte("\r\n"), []byte("\n"))
+	lines := strings.Split(string(data), "\n")
+	return lines
+}
+
+func main() {
+	success := true
+
+	success = test([]string{""}, 0) && success
+	success = test([]string{"", ""}, 0) && success
+
+	success = test([]string{"."}, 0) && success
+	success = test([]string{"..", ".."}, 0) && success
+	success = test([]string{"..."}, 0) && success
+	success = test([]string{"...", "..."}, 0) && success
+	success = test([]string{"...", "...", "..."}, 0) && success
+
+	success = test([]string{"@"}, 1) && success
+	success = test([]string{"@@"}, 2) && success
+	success = test([]string{"@.", ".."}, 1) && success
+	success = test([]string{".@", ".."}, 1) && success
+	success = test([]string{"..", "@."}, 1) && success
+	success = test([]string{"..", ".@"}, 1) && success
+	success = test([]string{"@@", ".."}, 2) && success
+	success = test([]string{"@.", "@."}, 2) && success
+	success = test([]string{"@.", ".@"}, 2) && success
+	success = test([]string{".@", "@."}, 2) && success
+	success = test([]string{".@", ".@"}, 2) && success
+	success = test([]string{"..", "@@"}, 2) && success
+	success = test([]string{"@@", "@."}, 3) && success
+	success = test([]string{"@@", ".@"}, 3) && success
+	success = test([]string{"@.", "@@"}, 3) && success
+
+	success = test([]string{".@.", "...", "..."}, 1) && success
+	success = test([]string{"...", ".@.", "..."}, 1) && success
+	success = test([]string{"...", "...", ".@."}, 1) && success
+	success = test([]string{"...", "@..", "..."}, 1) && success
+	success = test([]string{"...", "..@", "..."}, 1) && success
+	success = test([]string{"...", "@.@", "..."}, 2) && success
+	success = test([]string{".@.", "@@@", ".@."}, 5) && success
+
+	success = test([]string{
+		"..@@.@@@@.",
+		"@@@.@.@.@@",
+		"@@@@@.@.@@",
+		"@.@@@@..@.",
+		"@@.@@@@.@@",
+		".@@@@@@@.@",
+		".@.@.@.@@@",
+		"@.@@@.@@@@",
+		".@@@@@@@@.",
+		"@.@.@@@.@.",
+	}, 43) && success
+
+	if success {
+		fmt.Printf("-=-=-=-=-=-=-=-=-=-=-=-=-\n✅All tests passed!\n")
+	} else {
+		fmt.Printf("-=-=-=-=-=-=-=-=-=-=-=-=-\n❌Some tests failed!\n")
+		return
+	}
+
+	input := readInput("input.txt")
+
+	result := run(input)
+	fmt.Printf("Result: %d\n", result)
+}
